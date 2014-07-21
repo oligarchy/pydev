e427d9cab03138e42bf4edbedac7015ed05b689b
Justin <justin.joyce@verizon.net>
2014-07-21 14:30:13 -0400

    added new main file for settings up test data

--- pysort/mergesort.py
+++ pysort/mergesort.py
@@ -1,6 +1,4 @@
-import sys
-try:
-	total = sum(int(arg) for arg in sys.argv[1:])
-	print 'sum =', total
-except ValueError:
-	print 'Please supply integer arguments'
+def Sort(inputList):
+	# public function
+def __choosePivot(inputList):
+	# private function
