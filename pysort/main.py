4c7d7fb560a5a6d7865166c96d14c51cb6c6f329
Justin <justin.joyce@verizon.net>
2014-07-21 16:07:57 -0400

    starting fleshing out the mergesort

--- pysort/main.py
+++ pysort/main.py
@@ -1,1 +1,20 @@
 import mergesort
+words = ['zebra', 'cat', 'banana', 'sultan', 'apple']
+sortedWords = []
+
+numbers = [3, 8, 11, 7, 4, 55, 22, 99]
+sortedNumbers = []
+
+print 'First we try to sort numbers.'
+sortedNumbers = mergesort.Sort(numbers)
+
+for number in sortedNumbers:
+	print repr(number)
+
+print 'Then we try to sort words.'
+sortedWords = mergesort.Sort(words)
+
+for word in sortedWords:
+	print word
+
+# it is really hard trying to ignore the muscle memory to put semicolons at the end of each line.
--- pysort/mergesort.py
+++ pysort/mergesort.py
@@ -1,4 +1,59 @@
+import sys
+
 def Sort(inputList):
 	# public function
-def __choosePivot(inputList):
+	print 'You have ' + repr(len(inputList)) + ' items to sort.'
+
+	# Our base case.  A list of 1 or less is considered sorted.
+	if len(inputList) <= 0:
+		return inputList
+
+	# Find the mid point in the list, using int() to round in this case
+	# although there is probably a better way.
+	mid = int(len(inputList) / 2)
+	print 'Mid is: ' + repr(mid)
+
+	if mid < 2:
+		return inputList
+ 
+ 	# Initialize our split lists
+	left = []
+	right = []
+
+	# Copy the splits to their new homes.
+	left = __copySubArray(inputList, 0, mid - 1)
+	right = __copySubArray(inputList, mid, len(inputList))
+
+	# Recursion!
+	left = Sort(left)
+	right = Sort(right)
+
+	return __merge(left, right)
+
+def __copySubArray(source, start, end):
 	# private function
+	
+	# Same base case as caller, 1 or less is sorted.
+	if len(source) < 2:
+		return source
+
+	destination = []
+
+	for i in range(start, end):
+		destination.append(source[i])
+	return destination
+
+def __merge(a, b):
+	merged = []
+
+	#while (len(a) > 0) or (len(b) > 0)
+
+
+	return merged
+
+def __split(a, b, start, end):
+	if (end - start) < 2:
+		return
+
+	mid = int((start + end) / 2)
+	print repr(mid)
