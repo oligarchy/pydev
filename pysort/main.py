59133a2fbf81fa62654d773bad2b5915dacd9ae3
Justin <justin.joyce@verizon.net>
2014-07-22 09:35:48 -0400

    finished the merge sort, and it works!

--- pysort/main.py
+++ pysort/main.py
@@ -5,7 +5,7 @@ sortedWords = []
 numbers = [3, 8, 11, 7, 4, 55, 22, 99]
 sortedNumbers = []
 
-print 'First we try to sort numbers.'
+# print 'First we try to sort numbers.'
 sortedNumbers = mergesort.Sort(numbers)
 
 for number in sortedNumbers:
@@ -15,6 +15,6 @@ print 'Then we try to sort words.'
 sortedWords = mergesort.Sort(words)
 
 for word in sortedWords:
-	print word
+ 	print word
 
 # it is really hard trying to ignore the muscle memory to put semicolons at the end of each line.
--- pysort/mergesort.py
+++ pysort/mergesort.py
@@ -2,26 +2,23 @@ import sys
 
 def Sort(inputList):
 	# public function
-	print 'You have ' + repr(len(inputList)) + ' items to sort.'
+	# print 'You have ' + repr(len(inputList)) + ' items to sort.'
 
 	# Our base case.  A list of 1 or less is considered sorted.
-	if len(inputList) <= 0:
+	if len(inputList) <= 1 :
 		return inputList
 
 	# Find the mid point in the list, using int() to round in this case
 	# although there is probably a better way.
-	mid = int(len(inputList) / 2)
-	print 'Mid is: ' + repr(mid)
-
-	if mid < 2:
-		return inputList
+	mid = int(round(len(inputList) / 2))
+	# print 'Mid is: ' + repr(mid)
  
  	# Initialize our split lists
 	left = []
 	right = []
 
 	# Copy the splits to their new homes.
-	left = __copySubArray(inputList, 0, mid - 1)
+	left = __copySubArray(inputList, 0, mid )
 	right = __copySubArray(inputList, mid, len(inputList))
 
 	# Recursion!
@@ -31,8 +28,6 @@ def Sort(inputList):
 	return __merge(left, right)
 
 def __copySubArray(source, start, end):
-	# private function
-	
 	# Same base case as caller, 1 or less is sorted.
 	if len(source) < 2:
 		return source
@@ -46,14 +41,28 @@ def __copySubArray(source, start, end):
 def __merge(a, b):
 	merged = []
 
-	#while (len(a) > 0) or (len(b) > 0)
-
+	while (len(a) > 0) or (len(b) > 0) :
+		if len(a) > 0 and len(b) > 0 :
+			if a[0] <= b[0] :
+				merged.append(a[0])
+				a = __rest(a)
+			else :
+				merged.append(b[0])
+				b = __rest(b)
+		elif len(a) > 0 :
+			merged.append(a[0])
+			a = __rest(a)
+		elif len(b) > 0 :
+			merged.append(b[0])
+			b = __rest(b)
+		# print 'len(a): ' + repr(len(a)) + ' len(b)' + repr(len(b))
 
 	return merged
 
-def __split(a, b, start, end):
-	if (end - start) < 2:
-		return
+def __rest(inputList):
+	theRest = []
 
-	mid = int((start + end) / 2)
-	print repr(mid)
+	for i in range(1, len(inputList)) :
+		theRest.append(inputList[i])
+
+	return theRest
