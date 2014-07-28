36ff7bcf5f1e5c61ca1cc7d772096bc9fd9170ab
Justin <justin.joyce@verizon.net>
2014-07-28 15:47:49 -0400

    updated quicksort, not quite working

--- pysort/main.py
+++ pysort/main.py
@@ -7,12 +7,15 @@ sortedWords = []
 numbers = [3, 8, 11, 7, 4, 55, 22, 99]
 sortedNumbers = []
 
+qsortWords = quicksort.QuickSort(words)
+qsortNumbers = quicksort.QuickSort(numbers)
+
 print 'First we try to Quick sort numbers.'
-sortedNumbers = quicksort.Sort(numbers, 0, len(numbers))
+sortedNumbers = qsortNumbers.Sort(numbers, 0, len(numbers))
 
 
 print 'Then we try to quick sort words'
-sortedWords = quicksort.Sort(words, 0, len(words))
+sortedWords = qsortWords.Sort(words, 0, len(words))
 
 #print 'First we try to Merge sort numbers.'
 #sortedNumbers = mergesort.Sort(numbers)
--- pysort/quicksort.py
+++ pysort/quicksort.py
@@ -1,20 +1,48 @@
 import sys
 
-def Sort(inputList, start, end) :
-	if len(inputList) <= 1 :
+class QuickSort :
+	"I wanted to do a simple test of making a class"
+
+	toSort = []
+
+	def __init__(self, inputList) :
+		toSort = inputList
+
+	def Sort(self, inputList, start, end) :
+		if len(inputList) <= 1 :
+			return inputList
+
+		self.toSort = inputList
+		sortedList = []
+
+		if start < end :
+			partition = self.__partition(inputList, start, end)
+			sortedList = self.Sort(inputList, start, partition - 1)
+			sortedList = self.Sort(inputList, partition + 1, end)
+
+		return sortedList
+
+	def __partition(self, inputList, start, end) :
+		pivotIndex = self.__choosePivot(inputList, start, end - 1)
+		pivotValue = inputList[pivotIndex]
+
+		inputList = self.__swap(inputList, pivotIndex, end - 1)
+		
+		for i in range(start, end - 1) :
+			if inputList[i] <= pivotValue :
+				inputList = self.__swap(inputList, i, pivotIndex)
+				start = start + 1
+
+		return pivotIndex
+
+	def __choosePivot(self, inputList, start, end) : 
+		return int(len(inputList) / 2)
+
+	def __swap(self, inputList, a, b) :
+		print("Swap: " + repr(a) + ":" + repr(b))
+
+		leftValue = inputList[a]
+		inputList[a] = inputList[b]
+		inputList[b] = leftValue
+
 		return inputList
-
-	if start < end :
-		print 'do work'
-
-	return inputList
-
-def __partition(inputList, start, end) :
-	return inputList
-
-def __swap(inputList, a, b) :
-	leftValue = inputList[a]
-	inputList[a] = inputList[b]
-	inputList[b] = leftValue
-
-	return inputList
