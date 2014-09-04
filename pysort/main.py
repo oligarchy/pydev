47bcd5435d54394649a0434f636866f5588d7722
Justin <justin.joyce@verizon.net>
2014-09-04 11:41:34 -0400

    new kata for quicksort

--- pysort/main.py
+++ pysort/main.py
@@ -1,6 +1,6 @@
 import mergesort
 # import quicksort
-import quicksort20140827
+import quicksort20140904
 #import heap
 
 words = ['zebra', 'cat', 'banana', 'sultan', 'apple', 'cat', 'aardvark', 'hippo', 'dingo']
@@ -9,8 +9,8 @@ sortedWords = []
 numbers = [3, 8, 11, 7, 4, 55, 22, 99, 22, 13, 45, 31]
 sortedNumbers = []
 
-sortedWords = quicksort20140827.Sort(words, 0, len(words) - 1)
-sortedNumbers = quicksort20140827.Sort(numbers, 0, len(numbers) - 1)
+sortedWords = quicksort20140904.Sort(words, 0, len(words) - 1)
+sortedNumbers = quicksort20140904.Sort(numbers, 0, len(numbers) - 1)
 
 #print 'First we try to Quick sort numbers.'
 #sortedNumbers = qsortNumbers.Sort(numbers, 0, len(numbers) - 1)
---
+++ pysort/quicksort20140904.py
@@ -0,0 +1,40 @@
+import sys
+
+def Sort(input, low, high) :
+	if len(input) <= 1 :
+		return input
+
+	if low < high :
+		pivot = __partition(input, low, high)
+		input = Sort(input, low, pivot - 1)
+		input = Sort(input, pivot + 1, high)
+
+	return input
+
+def __partition(input, low, high) :
+	pivotIndex = __choosePivot(input, low, high)
+	pivotValue = input[pivotIndex]
+
+	input = __swap(input, pivotIndex, high)
+
+	storeIndex = low
+
+	for i in range(low, high) :
+		if input[i] < pivotValue :
+			input = __swap(input, i, storeIndex)
+			storeIndex += 1
+
+	input = __swap(input, storeIndex, high)
+	return storeIndex
+
+
+def __choosePivot(input, low, high) :
+	pivotMean = int((low + high) / 2)
+	return pivotMean
+
+def __swap(input, first, second) :
+	temp = input[first]
+	input[first] = input[second]
+	input[second] = temp
+
+	return input
