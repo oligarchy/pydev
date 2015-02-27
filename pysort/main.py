174d80b4487687edce599c2071a1bb6cc5476c2b
Justin <justin.joyce@verizon.net>
2015-02-27 13:06:57 -0500

    added new kata and some error checking in main file

--- pysort/main.py
+++ pysort/main.py
@@ -1,6 +1,6 @@
 import mergesort
 # import quicksort
-import quicksort20140904
+import quicksort20150227
 #import heap
 
 words = ['zebra', 'cat', 'banana', 'sultan', 'apple', 'cat', 'aardvark', 'hippo', 'dingo']
@@ -9,8 +9,8 @@ sortedWords = []
 numbers = [3, 8, 11, 7, 4, 55, 22, 99, 22, 13, 45, 31]
 sortedNumbers = []
 
-sortedWords = quicksort20140904.Sort(words, 0, len(words) - 1)
-sortedNumbers = quicksort20140904.Sort(numbers, 0, len(numbers) - 1)
+sortedWords = quicksort20150227.Sort(words, 0, len(words) - 1)
+sortedNumbers = quicksort20150227.Sort(numbers, 0, len(numbers) - 1)
 
 #print 'First we try to Quick sort numbers.'
 #sortedNumbers = qsortNumbers.Sort(numbers, 0, len(numbers) - 1)
@@ -22,13 +22,19 @@ sortedNumbers = quicksort20140904.Sort(numbers, 0, len(numbers) - 1)
 #print 'First we try to Merge sort numbers.'
 #sortedNumbers = mergesort.Sort(numbers)
 
-for number in sortedNumbers:
-	print repr(number)
+if sortedNumbers != None and len(sortedNumbers) != 0 :
+	for number in sortedNumbers:
+		print(repr(number))
+else :
+	print("No sorted numbers.")
 
 #print 'Then we try to Merge sort words.'
 #sortedWords = mergesort.Sort(words)
 
-for word in sortedWords:
- 	print word
+if sortedWords != None and len(sortedWords) != 0 :
+	for word in sortedWords:
+	 	print(word)
+else :
+	print("No sorted words.")
 
 # semi colons and braces are for chumps
---
+++ pysort/quicksort20150227.py
@@ -0,0 +1,13 @@
+import sys
+
+def Sort(input, low, high) :
+ 	pass
+
+def __partition(input, low, high) :
+	pass
+
+def __choosePivot(input, low, high) :
+	pass
+
+def __swap(input, first, second) :
+	pass
